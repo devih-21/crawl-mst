b820c41f605552ef99c9ecc9d3f53ca5eee50208
devih-21 <vinh20112001@gmail.com>
2024-06-01 22:43:36 +0700

    feat: update crawl by provide

--- crawl.js
+++ crawl.js
@@ -1,64 +1,121 @@
 const { JSDOM } = require("jsdom");
 const fs = require("fs");
 
-const url =
-  "https://masothue.com/3002273706-cong-ty-tnhh-thuong-mai-va-dich-vu-ky-thuat-co-dien-hong-son-thuy";
+const mainURL = "https://masothue.com";
 
-fetch(url)
-  .then((response) => response.text())
-  .then((html) => {
-    const { document } = new JSDOM(html).window;
+const urlProvide =
+  "https://masothue.com/tra-cuu-ma-so-thue-theo-tinh/ha-tinh-342";
 
-    const table1 = document.querySelector(".table-taxinfo");
-    const table2 = document.querySelector(".table");
+const getListPaging = async () => {
+  let listPaging = await fetch(urlProvide)
+    .then((response) => response.text())
+    .then((html) => {
+      let listPaging = [];
+      const { document } = new JSDOM(html).window;
+      const uiTablePagination = document.querySelector(".page-numbers");
+      const pagination = uiTablePagination.querySelectorAll("li");
+      for (let i = 0; i < pagination.length; i++) {
+        listPaging.push(pagination[i].textContent);
+      }
+      return listPaging;
+    });
+  return listPaging;
+};
 
-    const data1 = {};
-    const data2 = {};
+const getListCompany = async (listPaging) => {
+  let listCompany = await Promise.all(
+    listPaging.map(async (page) => {
+      const response = await fetch(urlProvide + "?page=" + page);
+      const html = await response.text();
+      const { document } = new JSDOM(html).window;
+      let companyList = [];
 
-    if (table1) {
-      const rows1 = table1.querySelectorAll("tr");
+      let domCompany = document.querySelector(".tax-listing");
+      let linkDOM = domCompany.querySelectorAll("h3");
+      for (let i = 0; i < linkDOM.length; i++) {
+        companyList.push(
+          mainURL + linkDOM[i].getElementsByTagName("a")[0].href
+        );
+      }
 
-      rows1.forEach((row) => {
-        const cells = row.querySelectorAll("td");
+      return companyList;
+    })
+  );
 
-        if (cells.length === 2) {
-          const key = cells[0]?.textContent?.trim();
-          const value =
-            cells?.[1]?.textContent ||
-            cells?.[1]?.querySelector(".copy")?.textContent?.trim();
+  return listCompany.flat();
+};
 
-          data1[key] = value;
-        }
-      });
-    } else {
-      console.log("Không tìm thấy bảng dữ liệu 1.");
-    }
+const getCompanyInfoByUrl = async (url) => {
+  await fetch(url)
+    .then((response) => response.text())
+    .then((html) => {
+      const { document } = new JSDOM(html).window;
 
-    if (table2) {
-      const rows2 = table2.querySelectorAll("tr");
+      const table1 = document.querySelector(".table-taxinfo");
+      const table2 = document.querySelector(".table");
 
-      rows2.forEach((row) => {
-        const cells = row.querySelectorAll("td");
+      const data1 = {};
+      const data2 = {};
 
-        if (cells.length === 2) {
-          const key = cells[0]?.textContent?.trim();
-          const value =
-            cells?.[1]?.textContent.trim() ||
-            cells?.[1]?.querySelector(".copy")?.textContent?.trim();
+      if (table1) {
+        const rows1 = table1.querySelectorAll("tr");
 
-          data2[key] = value;
-        }
-      });
-    } else {
-      console.log("Không tìm thấy bảng dữ liệu 2.");
-    }
+        rows1.forEach((row) => {
+          const cells = row.querySelectorAll("td");
 
-    fs.writeFileSync(
-      url.split("/").slice(-1).pop().replace(/-/g, "_") + ".json",
-      JSON.stringify({ companyInfo: data1, industryInfo: data2 }, null, 2)
-    );
-  })
+          if (cells.length === 2) {
+            const key = cells[0]?.textContent?.trim();
+            const value =
+              cells?.[1]?.textContent ||
+              cells?.[1]?.querySelector(".copy")?.textContent?.trim();
 
-  .catch((error) => {
-    console.error("Lỗi khi lấy dữ liệu:", error);
-  });
+            data1[key] = value;
+          }
+        });
+      } else {
+        console.log("Không tìm thấy bảng dữ liệu 1.");
+      }
+
+      if (table2) {
+        const rows2 = table2.querySelectorAll("tr");
+
+        rows2.forEach((row) => {
+          const cells = row.querySelectorAll("td");
+
+          if (cells.length === 2) {
+            const key = cells[0]?.textContent?.trim();
+            const value =
+              cells?.[1]?.textContent.trim() ||
+              cells?.[1]?.querySelector(".copy")?.textContent?.trim();
+
+            data2[key] = value;
+          }
+        });
+      } else {
+        console.log("Không tìm thấy bảng dữ liệu 2.");
+      }
+
+      fs.writeFileSync(
+        "./result/" +
+          url.split("/").slice(-1).pop().replace(/-/g, "_") +
+          ".json",
+        JSON.stringify({ companyInfo: data1, industryInfo: data2 }, null, 2)
+      );
+    })
+    .catch((error) => {
+      console.error("Lỗi khi lấy dữ liệu:", error);
+    });
+};
+
+const main = async () => {
+  const listPaging = await getListPaging();
+  const listCompany = await getListCompany(listPaging);
+
+  await Promise.all(
+    listCompany.map(async (company) => {
+      await getCompanyInfoByUrl(company);
+    })
+  );
+};
+
+main();
